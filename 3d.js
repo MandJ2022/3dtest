dba97292b6dbb0bd325dd7389cd873b0ca9db892
MandJ2022 <96705392+MandJ2022@users.noreply.github.com>
2023-03-14 14:34:23 +0100

    Add files via upload

---
+++ 3d.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector('.canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext('2d');
+const frameCount = 66;
+
+const currentFrame = (index) => `./pic/${(index + 1).toString()}.jpg`;
+const images = [];
+let ball = {frame : 0};
+
+for(let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+    images.push(img);
+}
+
+
+gsap.to(ball, {
+    frame: frameCount - 1,
+    snap: 'frame',
+    ease: 'none',
+    scrollTrigger: {
+        scrub: true,
+        pin: 'canvas',
+        end: '500%',
+    },
+    onUpdate: render,
+});
+
+images[0].onload = render;
+
+function render() {
+    context.clearRect(1,1, canvas.width, canvas.height);
+    context.drawImage(images[ball.frame], 0, 0);
+}
